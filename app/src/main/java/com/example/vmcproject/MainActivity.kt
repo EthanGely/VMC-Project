bb8736af7cf35f8341a77c0642479e28d87d4836
Insu-qg <quent.grange@gmail.com>
2025-03-06 09:45:12 +0100

    Add initial project setup with drawable resources and configuration files

--- app/src/main/java/com/example/vmcproject/MainActivity.kt
+++ app/src/main/java/com/example/vmcproject/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.vmcproject
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val powerButton: ImageButton = findViewById(R.id.powerButton)
+        powerButton.setOnClickListener {
+            Toast.makeText(this, "Bouton d'alimentation pressé", Toast.LENGTH_SHORT).show()
         }
     }
 }
